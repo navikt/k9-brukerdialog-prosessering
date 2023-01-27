e3a8aefda360ca867c84cad7becf26bbb3c241e3
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-01-27 14:44:59 +0100

    Bruker global variabel for navn på PSB kafka streams.

--- src/main/kotlin/no/nav/k9brukerdialogprosessering/pleiepengersyktbarn/PSBKafkaStreamsHealthIndicator.kt
+++ src/main/kotlin/no/nav/k9brukerdialogprosessering/pleiepengersyktbarn/PSBKafkaStreamsHealthIndicator.kt
@@ -22,7 +22,7 @@ class PSBKafkaStreamsHealthIndicator(
         return when (val kafkaStreams: KafkaStreams? = psbKafkaStreamsBuilder.kafkaStreams) {
             null -> Health.down().withDetail(PSB_KAFKA_STREAMS, "$PSB_KAFKA_STREAMS is null").build()
             else -> when (val state = kafkaStreams.state()) {
-                KafkaStreams.State.RUNNING -> Health.up().withDetail("kafkaStreams", "$PSB_KAFKA_STREAMS is in $state state").build()
+                KafkaStreams.State.RUNNING -> Health.up().withDetail(PSB_KAFKA_STREAMS, "$PSB_KAFKA_STREAMS is in $state state").build()
                 else -> Health.down().withDetail(PSB_KAFKA_STREAMS, "$PSB_KAFKA_STREAMS is in $state state").build()
             }
         }
