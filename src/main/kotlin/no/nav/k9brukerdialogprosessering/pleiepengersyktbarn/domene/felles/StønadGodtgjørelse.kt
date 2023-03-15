617cad33f7d3fabef0fe776956b988e1ce76dabc
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-03-15 13:17:37 +0100

    Fjerner struktur.

--- src/main/kotlin/no/nav/k9brukerdialogprosessering/pleiepengersyktbarn/domene/felles/StønadGodtgjørelse.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.k9brukerdialogprosessering.pleiepengersyktbarn.domene.felles
-
-import java.time.LocalDate
-
-data class StønadGodtgjørelse(
-    private val mottarStønadGodtgjørelse: Boolean,
-    private val startDato: LocalDate? = null,
-    private val slutttDato: LocalDate? = null
-) 
