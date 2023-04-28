58e355b7dace79a5da1ffd0f1f104dae3b9850fe
albrektsson <emil.albrektsson@nav.no>
2023-04-28 14:21:24 +0200

    Byttet fra deprecated compatibility till toolchains for å sette JDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,14 @@ group = "no.nav"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+		vendor.set(JvmVendorSpec.AMAZON)
+	}
+}
+
+kotlin {
+	jvmToolchain(17)
 }
 
 configurations {
