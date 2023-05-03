14a1472399c5d579a931f965a5f7e981470562ca
albrektsson <emil.albrektsson@nav.no>
2023-05-03 09:53:23 +0200

    Revertet kotlin plugins til 1.8.10 Lagt tilbake toolChains Fjernet annotationprocessor igen..

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.spring") version "1.8.21"
+	kotlin("jvm") version "1.8.10"
+	kotlin("plugin.spring") version "1.8.10"
 }
 
 group = "no.nav"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
 	}
 }
 
