287d2263c159a965092a64fc3387f23d0b34a75f
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2024-04-23 09:46:35 +0200

    Ikke vis pleietrengende-punkt om man ikke har dataen til det (#153)

--- src/test/kotlin/no/nav/k9brukerdialogprosessering/meldinger/ettersendelse/EttersendelsePdfGeneratorTest.kt
+++ src/test/kotlin/no/nav/k9brukerdialogprosessering/meldinger/ettersendelse/EttersendelsePdfGeneratorTest.kt
@@ -1,9 +1,13 @@
 package no.nav.k9brukerdialogprosessering.meldinger.ettersendelse
 
+import junit.framework.TestCase.assertTrue
 import no.nav.k9brukerdialogprosessering.meldinger.ettersendelse.domene.Søknadstype
 import no.nav.k9brukerdialogprosessering.meldinger.ettersendelse.utils.EttersendingUtils
 import no.nav.k9brukerdialogprosessering.pdf.PDFGenerator
+import no.nav.k9brukerdialogprosessering.pdf.PdfData
 import no.nav.k9brukerdialogprosessering.utils.PathUtils.pdfPath
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import java.io.File
@@ -23,6 +27,21 @@ class EttersendelsePdfGeneratorTest {
         genererOppsummeringsPdfer(søknadstype, true)
     }
 
+    @Test
+    fun `informasjon om barn inkluderes i pdf om det er pleietrengende`() {
+        val html = generator.genererHTML(EttersendingUtils.defaultEttersendelse().copy().pdfData())
+        Assertions.assertTrue(html.contains("Pleietrengende"))
+    }
+
+    @Test
+    fun `informasjon om barn inkluderes ikke i pdf om det ikke er pleietrengende`() {
+        val html = generator.genererHTML(EttersendingUtils.defaultEttersendelse().copy(
+                pleietrengende = null
+        ).pdfData())
+        Assertions.assertFalse(html.contains("Pleietrengende"))
+    }
+
+
     private companion object {
         const val PDF_PREFIX = "ettersendelse"
         private val generator = PDFGenerator()
@@ -31,10 +50,10 @@ class EttersendelsePdfGeneratorTest {
     private fun genererOppsummeringsPdfer(søknadstype: Søknadstype, writeBytes: Boolean) {
         val id = søknadstype.name.lowercase()
         val pdf = generator.genererPDF(
-            EttersendingUtils.defaultEttersendelse(id, ZonedDateTime.now()).copy(
-                søknadstype = søknadstype,
-                beskrivelse = null
-            ).pdfData()
+                EttersendingUtils.defaultEttersendelse(id, ZonedDateTime.now()).copy(
+                        søknadstype = søknadstype,
+                        beskrivelse = null
+                ).pdfData()
         )
 
         if (writeBytes) File(pdfPath(soknadId = id, prefix = PDF_PREFIX)).writeBytes(pdf)
