aa91055ac34371a84ce6d32430480a508a97fdc7
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-07-11 11:23:09 +0200

    Setter default headers.

--- src/main/kotlin/no/nav/k9brukerdialogprosessering/mellomlagring/dokument/K9MellomlagringClientsConfig.kt
+++ src/main/kotlin/no/nav/k9brukerdialogprosessering/mellomlagring/dokument/K9MellomlagringClientsConfig.kt
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.web.client.RestTemplate
 import java.time.Duration
 
@@ -34,6 +36,8 @@ class K9MellomlagringClientsConfig(
         return restTemplateBuilder
             .rootUri(baseUrl)
             .setReadTimeout(Duration.ofSeconds(20))
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .additionalInterceptors(
                 RestTemplateUtils.authorizationInterceptor(azureK9MellomlagringClientProperties, oAuth2AccessTokenService),
                 RestTemplateUtils.requestLoggerInterceptor(logger),
