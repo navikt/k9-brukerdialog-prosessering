0be1e0cb5e77484381092abd6543a16022a9ec91
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-08-05 13:00:00 +0200

    Fikser ytelse header navn.

--- src/main/kotlin/no/nav/k9brukerdialogprosessering/utils/NavHeaders.kt
+++ src/main/kotlin/no/nav/k9brukerdialogprosessering/utils/NavHeaders.kt
@@ -2,7 +2,7 @@ package no.nav.k9brukerdialogprosessering.utils
 
 object NavHeaders {
     const val BRUKERDIALOG_GIT_SHA = "X-Brukerdialog-Git-Sha"
-    const val BRUKERDIALOG_YTELSE = "X-Nav-Ytelse"
+    const val BRUKERDIALOG_YTELSE = "X-K9-Brukerdialog"
     const val X_CORRELATION_ID = "X-Correlation-ID"
     const val X_K9_YTELSE = "X-K9-Ytelse"
     const val PROBLEM_DETAILS = "problem-details"
