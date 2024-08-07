35b95c9a7675a516b6f9f7740642bcdefd28791f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-08-07 10:38:20 +0200

    Fjerner streng pattern fra zdt i CacheResponse

--- src/main/kotlin/no/nav/k9brukerdialogprosessering/mellomlagring/soknad/CacheResponse.kt
+++ src/main/kotlin/no/nav/k9brukerdialogprosessering/mellomlagring/soknad/CacheResponse.kt
@@ -6,7 +6,7 @@ import java.time.ZonedDateTime
 data class CacheResponse(
     val nøkkel: String,
     val verdi: String,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "UTC") val utløpsdato: ZonedDateTime,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "UTC") val opprettet: ZonedDateTime? = null,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "UTC") val endret: ZonedDateTime? = null
+    @JsonFormat(shape = JsonFormat.Shape.STRING) val utløpsdato: ZonedDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING) val opprettet: ZonedDateTime? = null,
+    @JsonFormat(shape = JsonFormat.Shape.STRING) val endret: ZonedDateTime? = null
 )
