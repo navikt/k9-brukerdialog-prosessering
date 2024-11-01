88614d108d553109ca5efc693c1d5c6f1496834f
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-11-01 11:04:42 +0100

    Fjern beskyttelse fra endepunkt
    Trengs vel strengt talt ikke i dette tilfellet

--- src/main/kotlin/no/nav/brukerdialog/oppslag/opplaeringsintitusjoner/OpplæringsinstitusjonerController.kt
+++ src/main/kotlin/no/nav/brukerdialog/oppslag/opplaeringsintitusjoner/OpplæringsinstitusjonerController.kt
@@ -1,19 +1,13 @@
 package no.nav.brukerdialog.oppslag.opplaeringsintitusjoner
 
 import kotlinx.coroutines.runBlocking
-import no.nav.brukerdialog.config.Issuers
 import no.nav.brukerdialog.integrasjon.k9selvbetjeningoppslag.OpplæringsinstitusjonerService
-import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.core.api.RequiredIssuers
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/oppslag/opplæringsinstitusjoner")
-@RequiredIssuers(
-    ProtectedWithClaims(issuer = Issuers.TOKEN_X, claimMap = ["acr=Level4"])
-)
 class OpplæringsinstitusjonerController(
     private val opplæringsinstitusjonerService: OpplæringsinstitusjonerService
 ) {
