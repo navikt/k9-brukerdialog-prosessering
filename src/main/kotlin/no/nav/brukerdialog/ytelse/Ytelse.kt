422dab55f0476e24c4912518aa3111de935330b0
Morten Osvik <morten.osvik@bekk.no>
2024-11-07 13:58:15 +0100

    Fiks heaer ytelse for opplæringspenger

--- src/main/kotlin/no/nav/brukerdialog/ytelse/Ytelse.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/Ytelse.kt
@@ -23,7 +23,7 @@ enum class Ytelse(val dialog: String) {
     PLEIEPENGER_SYKT_BARN("pleiepengesoknad"),
     ENDRINGSMELDING_PLEIEPENGER_SYKT_BARN("endringsmelding-pleiepenger"),
     DINE_PLEIEPENGER("dine-pleiepenger"),
-    OPPLARINGSPENGER("opplaeringspenger-soknad"),
+    OPPLARINGSPENGER("opplaringspenger-soknad"),
     UNGDOMSYTELSE("ungdomsytelse-deltaker")
     ;
 
