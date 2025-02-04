e5103342a09f68c600e2b18161e9dac4182c6561
Fredrik Garseg Mørk <fredrik.1997@hotmail.com>
2025-02-04 12:38:26 +0100

    Test-controller for familie-pdf i dev

---
+++ src/main/kotlin/no/nav/brukerdialog/integrasjon/familiepdf/PdfController.kt
@@ -0,0 +1,28 @@
+package no.nav.brukerdialog.integrasjon.familiepdf
+
+import kotlinx.coroutines.runBlocking
+import no.nav.brukerdialog.integrasjon.familiepdf.dto.FamiliePdfPostRequest
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.context.annotation.Profile
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/pdf")
+@Unprotected
+@Profile("dev-gcp")
+class PdfController(
+    private val familiePdfService: FamiliePdfService,
+) {
+    @PostMapping
+    fun lagPdf(
+        @RequestBody familiePdfRequestBody: FamiliePdfPostRequest,
+    ): ResponseEntity<ByteArray> =
+        runBlocking {
+            val pdfBytes = familiePdfService.lagPdfKvittering(familiePdfRequestBody)
+            ResponseEntity.ok(pdfBytes)
+        }
+}
