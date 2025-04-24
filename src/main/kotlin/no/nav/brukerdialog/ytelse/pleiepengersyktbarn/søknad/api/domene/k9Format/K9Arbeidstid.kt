46fbf1658ccde649d381ad7399dd9f5f327780ab
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-04-24 14:43:55 +0200

    Mapper arbeidstid for Omsorgsstønad.
    Legger til TODO for sammenslåing av arbeidstid for Frilans og Omsorgsstønad.

--- src/main/kotlin/no/nav/brukerdialog/ytelse/pleiepengersyktbarn/søknad/api/domene/k9Format/K9Arbeidstid.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/pleiepengersyktbarn/søknad/api/domene/k9Format/K9Arbeidstid.kt
@@ -3,6 +3,7 @@ package no.nav.brukerdialog.ytelse.pleiepengersyktbarn.søknad.api.domene.k9Form
 import no.nav.brukerdialog.ytelse.pleiepengersyktbarn.søknad.api.domene.Arbeidsgiver
 import no.nav.brukerdialog.ytelse.pleiepengersyktbarn.søknad.api.domene.PleiepengerSyktBarnSøknad
 import no.nav.k9.søknad.felles.type.Organisasjonsnummer
+import no.nav.k9.søknad.felles.type.Periode
 import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.Arbeidstaker
 import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.Arbeidstid
 import java.time.LocalDate
@@ -12,7 +13,15 @@ internal fun PleiepengerSyktBarnSøknad.byggK9Arbeidstid(): Arbeidstid {
 
         if(arbeidsgivere.isNotEmpty()) medArbeidstaker(arbeidsgivere.tilK9Arbeidstaker(fraOgMed, tilOgMed))
 
-        medFrilanserArbeidstid(frilans.k9ArbeidstidInfo(fraOgMed, tilOgMed))
+        val frilansArbeidstidInfo = frilans.k9ArbeidstidInfo(fraOgMed, tilOgMed)
+        val omsorgsstønadArbeidstidInfo = omsorgsstønad?.k9ArbeidstidInfo(Periode(fraOgMed, tilOgMed))
+        if (omsorgsstønadArbeidstidInfo == null) {
+            medFrilanserArbeidstid(frilansArbeidstidInfo)
+        } else {
+            // TODO: Slå sammen arbeidstidene til frilans og omsorgsstønad
+            medFrilanserArbeidstid(frilansArbeidstidInfo)
+        }
+
         selvstendigNæringsdrivende.arbeidsforhold?.let {
             medSelvstendigNæringsdrivendeArbeidstidInfo(selvstendigNæringsdrivende.k9ArbeidstidInfo(fraOgMed, tilOgMed))
         }
--- src/main/kotlin/no/nav/brukerdialog/ytelse/pleiepengersyktbarn/søknad/api/domene/omsorgsstønad/Omsorgsstønad.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/pleiepengersyktbarn/søknad/api/domene/omsorgsstønad/Omsorgsstønad.kt
@@ -2,6 +2,10 @@ package no.nav.brukerdialog.ytelse.pleiepengersyktbarn.søknad.api.domene.omsorg
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import no.nav.brukerdialog.ytelse.pleiepengersyktbarn.søknad.api.domene.arbeid.NULL_TIMER
+import no.nav.k9.søknad.felles.type.Periode
+import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.ArbeidstidInfo
+import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.ArbeidstidPeriodeInfo
 import java.time.Duration
 import java.time.LocalDate
 
@@ -19,12 +23,23 @@ import java.time.LocalDate
 sealed class Omsorgsstønad(
     open val type: OmsorgsstønadType,
     open val mottarOmsorgsstønad: Boolean,
-)
+) {
+    abstract fun k9ArbeidstidInfo(søknadsperiode: Periode): ArbeidstidInfo
+}
 
 data class OmsorgsstønadMottarIkke(
     override val type: OmsorgsstønadType,
-    override val mottarOmsorgsstønad: Boolean
-) : Omsorgsstønad(type = type, mottarOmsorgsstønad = mottarOmsorgsstønad)
+    override val mottarOmsorgsstønad: Boolean,
+) : Omsorgsstønad(type = type, mottarOmsorgsstønad = mottarOmsorgsstønad) {
+    override fun k9ArbeidstidInfo(søknadsperiode: Periode): ArbeidstidInfo = ArbeidstidInfo().medPerioder(
+        mapOf(
+            søknadsperiode to ArbeidstidPeriodeInfo()
+                .medFaktiskArbeidTimerPerDag(NULL_TIMER)
+                .medJobberNormaltTimerPerDag(NULL_TIMER)
+        )
+    )
+}
+
 
 data class OmsorgsstønadMottarDelerAvPerioden(
     override val type: OmsorgsstønadType,
@@ -32,13 +47,35 @@ data class OmsorgsstønadMottarDelerAvPerioden(
     val startdato: LocalDate? = null, // dato settes hvis bruker har valgt ja på at hen starter i perioden
     val sluttdato: LocalDate? = null, // dato settes hvis bruker har valgt nei på at hen starter i perioden
     val antallTimer: Duration, // Antall timer i uken en mottar omsorgsstønad
-) : Omsorgsstønad(type = type, mottarOmsorgsstønad = mottarOmsorgsstønad)
+) : Omsorgsstønad(type = type, mottarOmsorgsstønad = mottarOmsorgsstønad) {
+    override fun k9ArbeidstidInfo(søknadsperiode: Periode): ArbeidstidInfo {
+        val startdato = startdato ?: søknadsperiode.fraOgMed
+        val sluttdato = sluttdato ?: søknadsperiode.tilOgMed
+
+        return ArbeidstidInfo().medPerioder(
+            mapOf(
+                Periode(startdato, sluttdato) to ArbeidstidPeriodeInfo()
+                    .medFaktiskArbeidTimerPerDag(antallTimer)
+                    .medJobberNormaltTimerPerDag(antallTimer)
+            )
+        )
+    }
+
+}
 
 data class OmsorgsstønadMottarHelePerioden(
     override val type: OmsorgsstønadType,
     override val mottarOmsorgsstønad: Boolean,
     val antallTimer: Duration, // Antall timer i uken en mottar omsorgsstønad
-): Omsorgsstønad(type = type, mottarOmsorgsstønad = mottarOmsorgsstønad)
+) : Omsorgsstønad(type = type, mottarOmsorgsstønad = mottarOmsorgsstønad) {
+    override fun k9ArbeidstidInfo(søknadsperiode: Periode): ArbeidstidInfo = ArbeidstidInfo().medPerioder(
+        mapOf(
+            søknadsperiode to ArbeidstidPeriodeInfo()
+                .medFaktiskArbeidTimerPerDag(antallTimer)
+                .medJobberNormaltTimerPerDag(antallTimer)
+        )
+    )
+}
 
 enum class OmsorgsstønadType {
     MOTTAR_IKKE,
