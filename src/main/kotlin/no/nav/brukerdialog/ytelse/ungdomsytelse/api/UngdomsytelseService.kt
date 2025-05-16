b00209fcb75e997a473cdebab77ba3eab3bc7953
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-05-16 10:14:06 +0200

    Henter og bruker oppgave for inntektsrapportering.

--- src/main/kotlin/no/nav/brukerdialog/ytelse/ungdomsytelse/api/UngdomsytelseService.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/ungdomsytelse/api/UngdomsytelseService.kt
@@ -9,12 +9,16 @@ import no.nav.brukerdialog.integrasjon.ungdeltakelseopplyser.UngDeltakelseOpplys
 import no.nav.brukerdialog.metrikk.MetrikkService
 import no.nav.brukerdialog.utils.MDCUtil
 import no.nav.brukerdialog.utils.TokenUtils.personIdent
+import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.inntektsrapportering.OppgittInntektForPeriode
+import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.inntektsrapportering.UngPeriode
 import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.inntektsrapportering.UngdomsytelseInntektsrapportering
+import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.inntektsrapportering.UngdomsytelseInntektsrapporteringInnsending
 import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.oppgavebekreftelse.UngdomsytelseOppgavebekreftelseInnsending
 import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.oppgavebekreftelse.UngdomsytelseOppgavebekreftelse
 import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.soknad.Barn
 import no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.soknad.Ungdomsytelsesøknad
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
+import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.felles.InntektsrapporteringOppgavetypeDataDTO
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.*
@@ -48,13 +52,33 @@ class UngdomsytelseService(
     }
 
     suspend fun inntektrapportering(rapportetInntekt: UngdomsytelseInntektsrapportering, gitSha: String) {
-        val metadata = MetaInfo(correlationId = MDCUtil.callIdOrNew(), soknadDialogCommitSha = gitSha)
-        val cacheKey = "${springTokenValidationContextHolder.personIdent()}_${rapportetInntekt.ytelse()}"
+        val rapporterInntektOppgave = ungDeltakelseOpplyserService.hentOppgaveForDeltakelse(
+            UUID.fromString(rapportetInntekt.oppgaveReferanse)
+        )
 
-        logger.info(formaterStatuslogging(rapportetInntekt.ytelse(), rapportetInntekt.innsendingId(), "mottatt."))
+        val inntektsrapporteringOppgaveData = (rapporterInntektOppgave.oppgavetypeData as? InntektsrapporteringOppgavetypeDataDTO
+            ?: throw IllegalStateException("OppgavetypeData er ikke av type InntektsrapporteringOppgavetypeDataDTO"))
+
+        val inntektsrapporteringInnsending = UngdomsytelseInntektsrapporteringInnsending(
+            oppgaveReferanse = rapportetInntekt.oppgaveReferanse,
+            mottatt = rapportetInntekt.mottatt,
+            oppgittInntektForPeriode = OppgittInntektForPeriode(
+                arbeidstakerOgFrilansInntekt = rapportetInntekt.oppgittInntekt.arbeidstakerOgFrilansInntekt,
+                periodeForInntekt = UngPeriode(
+                    fraOgMed = inntektsrapporteringOppgaveData.fraOgMed,
+                    tilOgMed = inntektsrapporteringOppgaveData.tilOgMed
+                )
+            ),
+            harBekreftetInntekt = rapportetInntekt.harBekreftetInntekt,
+        )
+
+        val metadata = MetaInfo(correlationId = MDCUtil.callIdOrNew(), soknadDialogCommitSha = gitSha)
+        val cacheKey = "${springTokenValidationContextHolder.personIdent()}_${inntektsrapporteringInnsending.ytelse()}"
+
+        logger.info(formaterStatuslogging(inntektsrapporteringInnsending.ytelse(), inntektsrapporteringInnsending.innsendingId(), "mottatt."))
         duplikatInnsendingSjekker.forsikreIkkeDuplikatInnsending(cacheKey)
-        innsendingService.registrer(rapportetInntekt, metadata)
-        metrikkService.registrerMottattInnsending(rapportetInntekt.ytelse())
+        innsendingService.registrer(inntektsrapporteringInnsending, metadata)
+        metrikkService.registrerMottattInnsending(inntektsrapporteringInnsending.ytelse())
     }
 
     suspend fun oppgavebekreftelse(oppgavebekreftelse: UngdomsytelseOppgavebekreftelse, gitSha: String) {
--- src/main/kotlin/no/nav/brukerdialog/ytelse/ungdomsytelse/api/domene/inntektsrapportering/UngdomsytelseInntektsrapportering.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/ungdomsytelse/api/domene/inntektsrapportering/UngdomsytelseInntektsrapportering.kt
@@ -3,24 +3,8 @@ package no.nav.brukerdialog.ytelse.ungdomsytelse.api.domene.inntektsrapportering
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.Valid
 import jakarta.validation.constraints.AssertTrue
-import no.nav.brukerdialog.common.MetaInfo
-import no.nav.brukerdialog.domenetjenester.innsending.Innsending
-import no.nav.brukerdialog.oppslag.soker.Søker
-import no.nav.brukerdialog.ytelse.Ytelse
-import no.nav.k9.søknad.Søknad
-import no.nav.k9.søknad.SøknadValidator
-import no.nav.k9.søknad.felles.Kildesystem
-import no.nav.k9.søknad.felles.Versjon
-import no.nav.k9.søknad.felles.type.Språk
-import no.nav.k9.søknad.felles.type.SøknadId
-import no.nav.k9.søknad.ytelse.ung.v1.inntekt.OppgittInntekt
-import no.nav.k9.søknad.ytelse.ung.v1.UngSøknadstype
-import no.nav.k9.søknad.ytelse.ung.v1.Ungdomsytelse
-import no.nav.k9.søknad.ytelse.ung.v1.UngdomsytelseSøknadValidator
-import java.net.URL
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
-import no.nav.k9.søknad.Søknad as UngSøknad
 
 data class UngdomsytelseInntektsrapportering(
     @field:org.hibernate.validator.constraints.UUID(message = "Forventet gyldig UUID, men var '\${validatedValue}'")
@@ -29,52 +13,8 @@ data class UngdomsytelseInntektsrapportering(
     @Schema(hidden = true)
     val mottatt: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
 
-    @field:Valid val oppgittInntektForPeriode: OppgittInntektForPeriode,
+    @field:Valid val oppgittInntekt: OppgittInntekt,
 
     @field:AssertTrue(message = "Inntektsopplysningene må bekreftes for å kunne rapportere")
-    val harBekreftetInntekt: Boolean,
-
-    ) : Innsending {
-    companion object {
-        private val K9_SØKNAD_VERSJON = Versjon.of("1.0.0")
-    }
-
-    override fun somKomplettSøknad(
-        søker: Søker,
-        k9Format: no.nav.k9.søknad.Innsending?,
-        titler: List<String>,
-    ): UngdomsytelseKomplettInntektsrapportering {
-        requireNotNull(k9Format)
-        return UngdomsytelseKomplettInntektsrapportering(
-            oppgaveReferanse = oppgaveReferanse,
-            mottatt = mottatt,
-            søker = søker,
-            oppgittInntektForPeriode = oppgittInntektForPeriode,
-            harBekreftetInntekt = harBekreftetInntekt,
-            k9Format = k9Format as UngSøknad
-        )
-    }
-
-    override fun valider() = mutableListOf<String>()
-
-    override fun somK9Format(søker: Søker, metadata: MetaInfo): UngSøknad {
-        val ytelse = Ungdomsytelse()
-            .medSøknadType(UngSøknadstype.RAPPORTERING_SØKNAD)
-            .medInntekter(OppgittInntekt(setOf(oppgittInntektForPeriode.somUngOppgittInntektForPeriode())))
-
-        return UngSøknad()
-            .medVersjon(K9_SØKNAD_VERSJON)
-            .medMottattDato(mottatt)
-            .medSpråk(Språk.NORSK_BOKMÅL)
-            .medSøknadId(SøknadId(oppgaveReferanse))
-            .medSøker(søker.somK9Søker())
-            .medYtelse(ytelse)
-            .medKildesystem(Kildesystem.SØKNADSDIALOG)
-    }
-
-    override fun søkerNorskIdent(): String? = null
-    override fun ytelse(): Ytelse = Ytelse.UNGDOMSYTELSE_INNTEKTSRAPPORTERING
-    override fun innsendingId(): String = oppgaveReferanse
-    override fun vedlegg(): List<URL> = mutableListOf()
-    override fun søknadValidator(): SøknadValidator<Søknad> = UngdomsytelseSøknadValidator()
-}
+    val harBekreftetInntekt: Boolean
+)
