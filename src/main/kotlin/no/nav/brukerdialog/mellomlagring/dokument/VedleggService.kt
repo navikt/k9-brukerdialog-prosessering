4da05f3dd68e420dec5861fe42f28a9cdd0c271d
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-07-01 16:02:44 +0200

    Skrur av virusskanning enn så lenge.

--- src/main/kotlin/no/nav/brukerdialog/mellomlagring/dokument/VedleggService.kt
+++ src/main/kotlin/no/nav/brukerdialog/mellomlagring/dokument/VedleggService.kt
@@ -15,7 +15,7 @@ class VedleggService(
             dokument = vedlegg.somDokument(personIdent),
             dokumentEier = DokumentEier(personIdent),
             medHold = false,
-            skannForVirus = true
+            skannForVirus = false // TODO: Skru på virusskanning før prodsetting.
         )
     }
 
