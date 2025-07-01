8546e2b7e714a7fd8fdbfbc11a21f97d8d991ba5
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-07-01 11:00:50 +0200

    Fikser krypteringssnøkkel

--- src/main/kotlin/no/nav/brukerdialog/mellomlagring/dokument/kryptering/KrypteringService.kt
+++ src/main/kotlin/no/nav/brukerdialog/mellomlagring/dokument/kryptering/KrypteringService.kt
@@ -10,17 +10,10 @@ import java.util.*
 
 @Service
 class KrypteringService(
-    @Value("\${no.nav.mellomlagring.kryptering.passord.krypteringsnøkkel}") private val krypteringsnøkkel: String,
-    @Value("\${no.nav.mellomlagring.kryptering.passord.dekrypteringsnøkler}") private val dekrypteringsnøkler: Map<Int, String>,
+    @Value("\${no.nav.mellomlagring.kryptering.passord.krypteringsnøkkel}") private val krypteringsnøkkel: String
 ) {
     private companion object {
         private val logger = LoggerFactory.getLogger(KrypteringService::class.java)
-    }
-
-    init {
-        logger.info("Genererer ID'er med Nøkkel ID = $krypteringsnøkkel")
-        logger.info("Decrypterer med ${dekrypteringsnøkler.size} mulige Nøkkel ID'er:")
-        dekrypteringsnøkler.forEach { logger.info("${it.key}") }
     }
 
     fun krypter(id: String, plainText: String, dokumentEier: DokumentEier): String {
@@ -29,7 +22,7 @@ class KrypteringService(
         logger.trace("Krypterer med Nøkkel ID $keyId")
 
         return KrypteringUtil(
-            passphrase = hentPassord(keyId),
+            passphrase = krypteringsnøkkel,
             iv = dokumentEier.eiersFødselsnummer
         ).krypter(plainText)
     }
@@ -39,7 +32,7 @@ class KrypteringService(
         val keyId = hentNøkkelId(id)
         logger.trace("Dekrypterer med Nøkkel ID $keyId")
 
-        return KrypteringUtil(passphrase = hentPassord(keyId), iv = dokumentEier.eiersFødselsnummer).dekrypter(encrypted)
+        return KrypteringUtil(passphrase = krypteringsnøkkel, iv = dokumentEier.eiersFødselsnummer).dekrypter(encrypted)
     }
 
     fun id(id: String = UUID.randomUUID().toString()): String {
@@ -62,11 +55,4 @@ class KrypteringService(
     private fun decodeId(id: String) = JWT.decode(if (id.endsWith(".")) id else "$id.")
 
     private fun hentNøkkelId(id: String) = decodeId(id).keyId.toInt()
-
-    private fun hentPassord(keyId: Int): String {
-        if (!dekrypteringsnøkler.containsKey(keyId)) {
-            throw IllegalStateException("Har inget passord tilgjengelig for Nøkkel ID $keyId. Får ikke gjort encrypt/decrypt.")
-        }
-        return dekrypteringsnøkler[keyId]!!
-    }
 }
