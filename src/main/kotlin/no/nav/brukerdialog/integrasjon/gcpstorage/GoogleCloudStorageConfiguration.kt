95914e9bef4cea0efff79bfcf74203b96787dcb7
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-08-18 14:59:41 +0200

    VTP WIP

--- src/main/kotlin/no/nav/brukerdialog/integrasjon/gcpstorage/GoogleCloudStorageConfiguration.kt
+++ src/main/kotlin/no/nav/brukerdialog/integrasjon/gcpstorage/GoogleCloudStorageConfiguration.kt
@@ -1,5 +1,6 @@
 package no.nav.brukerdialog.integrasjon.gcpstorage
 
+import com.google.cloud.NoCredentials
 import com.google.cloud.storage.Storage
 import com.google.cloud.storage.StorageOptions
 import org.springframework.context.annotation.Bean
@@ -7,11 +8,24 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile(value = ["dev-gcp", "prod-gcp"]) // Aktiv i dev-gcp og prod-gcp profiler
+@Profile(value = ["vtp", "dev-gcp", "prod-gcp"]) // Aktiv i dev-gcp og prod-gcp profiler
 class GoogleCloudStorageConfiguration {
 
-    @Bean
+    @Bean("storage")
+    @Profile(value = ["prod-gcp", "dev-gcp"])
     fun storage(): Storage {
         return StorageOptions.getDefaultInstance().service
+    }
+
+    @Bean("storage")
+    @Profile(value = ["vtp"])
+    fun storageNoCredentials(): Storage {
+        return StorageOptions.newBuilder()
+            .setProjectId("vtp")
+            // localhost
+            .setHost("https://localhost:4443") // Juster host om nødvendig for din lokale oppsett
+            .setCredentials(NoCredentials.getInstance())
+            .build()
+            .service
     }
 }
