7853a449904a32f72b2b64415d95a07417918c8e
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-08-19 02:06:42 -0700

    Legger til og konfigurerer vtp profil. (#353)
    * VTP WIP
    
    * Oppretter bucket om det ikke finnes fra før.

--- src/main/kotlin/no/nav/brukerdialog/integrasjon/gcpstorage/GoogleCloudStorageConfiguration.kt
+++ src/main/kotlin/no/nav/brukerdialog/integrasjon/gcpstorage/GoogleCloudStorageConfiguration.kt
@@ -1,17 +1,43 @@
 package no.nav.brukerdialog.integrasjon.gcpstorage
 
+import com.google.cloud.NoCredentials
+import com.google.cloud.storage.BucketInfo
 import com.google.cloud.storage.Storage
 import com.google.cloud.storage.StorageOptions
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile(value = ["dev-gcp", "prod-gcp"]) // Aktiv i dev-gcp og prod-gcp profiler
+@Profile(value = ["vtp", "dev-gcp", "prod-gcp"]) // Aktiv i dev-gcp og prod-gcp profiler
 class GoogleCloudStorageConfiguration {
+    private companion object {
+        private val logger = LoggerFactory.getLogger(GoogleCloudStorageConfiguration::class.java)
+    }
 
-    @Bean
+    @Bean("storage")
+    @Profile(value = ["prod-gcp", "dev-gcp"])
     fun storage(): Storage {
         return StorageOptions.getDefaultInstance().service
+    }
+
+    @Bean("storage")
+    @Profile(value = ["vtp"])
+    fun storageNoCredentials(): Storage {
+        val service = StorageOptions.newBuilder()
+            .setProjectId("vtp")
+            // localhost
+            .setHost("http://localhost:4443") // Juster host om nødvendig for din lokale oppsett
+            .setCredentials(NoCredentials.getInstance())
+            .build()
+            .service
+
+        service.get("k9-mellomlagring") ?: run {
+            // Hvis bøtten ikke finnes, oppretter vi den
+            logger.info("Bucket k9-mellomlagring ikke funnet. Oppretter ny bucket.")
+            service.create(BucketInfo.newBuilder("k9-mellomlagring").build())
+        }
+        return service
     }
 }
