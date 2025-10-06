ef8f1efa53253618a04db24b639064874b74b95b
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-10-06 13:25:19 +0200

    UNG - PDF endringer

--- src/main/kotlin/no/nav/brukerdialog/ytelse/ungdomsytelse/api/domene/oppgavebekreftelse/KomplettUngdomsytelseOppgaveDTO.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/ungdomsytelse/api/domene/oppgavebekreftelse/KomplettUngdomsytelseOppgaveDTO.kt
@@ -112,7 +112,7 @@ data class KomplettKontrollerRegisterInntektOppgaveTypeDataDTO(
         return inntektBekreftelse.build()
     }
 
-    override fun dokumentTittelSuffix(): String = "kontroll av innetekt"
+    override fun dokumentTittelSuffix(): String = "kontroll av inntekt"
 
     override fun somKomplettOppgave(oppgaveDTO: OppgaveDTO): KomplettUngdomsytelseOppgaveDTO {
         return KomplettKontrollerRegisterInntektOppgaveTypeDataDTO(
