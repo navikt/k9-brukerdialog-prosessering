3e09d595555f62a4ef8b63571079170c7ffb9984
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-03-06 08:52:33 +0100

    søknadid skal ikke eksponeres

--- src/main/kotlin/no/nav/brukerdialog/ytelse/aktivitetspenger/api/domene/soknad/Aktivitetspengersøknad.kt
+++ src/main/kotlin/no/nav/brukerdialog/ytelse/aktivitetspenger/api/domene/soknad/Aktivitetspengersøknad.kt
@@ -3,14 +3,13 @@ package no.nav.brukerdialog.ytelse.aktivitetspenger.api.domene.soknad
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.Valid
 import jakarta.validation.constraints.AssertTrue
-import org.hibernate.validator.constraints.UUID
 import java.time.LocalDate
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
 data class Aktivitetspengersøknad(
-    @field:UUID(message = "Forventet gyldig UUID, men var '\${validatedValue}'")
-    val søknadId: String,
+    @field:Schema(hidden = true)
+    val søknadId: String = java.util.UUID.randomUUID().toString(),
     val språk: String,
     @field:Schema(hidden = true)
     val mottatt: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
