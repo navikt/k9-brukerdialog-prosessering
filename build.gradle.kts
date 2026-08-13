a4fc804de9c5265e5b1707c3025db4bd53e69746
Ramin Esfandiari <ramin.esfandiari@nav.no>
2026-08-13 09:26:48 +0200

    Bump k9FormatVersion from 13.4.0 to 13.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ repositories {
 
 val tokenSupportVersion = "6.0.11"
 val jsonassertVersion = "1.5.3"
-val k9FormatVersion = "13.4.0"
+val k9FormatVersion = "13.4.1"
 val ungDeltakelseOpplyserVersjon = "2.11.3"
 val ungBrukerdialigApiVersjon = "1.0.7"
 val springMockkVersion = "5.0.1"
